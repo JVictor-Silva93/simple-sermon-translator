3afc3373d178e923e9e4a195f998fe095117bae2
victor <93vitinho2012@gmail.com>
2026-08-29 21:53:30 -0400

    #3 - WIP: Adding base linting and import tests.

---
+++ app/tests/test_sermon_app_import.py
@@ -0,0 +1,4 @@
+# app/tests/test_import.py
+
+def test_sermon_app_imports():
+    import sermon_app
---
+++ engine/tests/test_sermon_engine_import.py
@@ -0,0 +1,4 @@
+# engine/tests/test_import.py
+
+def test_sermon_engine_imports():
+    import sermon_engine
